45f02f7c2c96b41370cec62c48416294f6f1a987
Vlad Filippov <vlad.filippov@gmail.com>
2015-07-05 23:55:03 -0400

    feat(mailcheck): add mailcheck to experiments

---
+++ mailcheck/enabled.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  name: 'mailcheck is enable or disabled',
+  hypothesis: 'mailcheck will lead to higher confirmation rate of accounts',
+  startDate: '2015-01-01',
+  endDate: '2015-11-01',
+  subjectAttributes: ['uuid', 'isMetricsEnabled', 'forceMailcheck'],
+  independentVariables: ['mailcheckEnabled'],
+  eligibilityFunction: function (subject) {
+    if (subject) {
+      if (subject.forceMailcheck === 'true') {
+        return true;
+      }
+
+      if (subject.forceMailcheck === 'false') {
+        return false;
+      }
+
+      if (subject.isMetricsEnabled) {
+        return true;
+      }
+    }
+
+    return false;
+  },
+  groupingFunction: function (subject) {
+    var trial = this.bernoulliTrial(1, subject.uuid);
+
+    if (subject.forceMailcheck === 'true') {
+      trial = true;
+    }
+
+    return {
+      mailcheckEnabled: trial
+    };
+  }
+};
