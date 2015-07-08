88a49a0a1a25c9b8bdefc1d37c0768d266f3f666
Shane Tomlinson <stomlinson@mozilla.com>
2015-07-08 15:27:58 +0100

    chore(tests): Rename `uuid` to `uniqueUserId` for clarity.
    This goes along with mozilla/fxa-content-server#2698

--- metrics/errors.js
+++ metrics/errors.js
@@ -3,12 +3,12 @@
 module.exports = {
   name: 'enables JavaScript Sentry error metrics',
   startDate: '2015-01-01',
-  subjectAttributes: ['uuid', 'env'],
+  subjectAttributes: ['uniqueUserId', 'env'],
   independentVariables: ['sentryEnabled'],
   eligibilityFunction: function (subject) {
     var sampleRate = subject.env === 'development' ? 1.0 : 0.3;
 
-    return !! (subject.env && subject.uuid && this.bernoulliTrial(sampleRate, subject.uuid));
+    return !! (subject.env && subject.uniqueUserId && this.bernoulliTrial(sampleRate, subject.uniqueUserId));
   },
   groupingFunction: function () {
     return {
